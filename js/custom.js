fa013fb872849a7a2953f84de5e18abd3fd63b56
Christiaan <info@treedevelop.com>
2013-10-30 21:58:36 +0100

    Layout version 1

--- js/custom.js
+++ js/custom.js
@@ -9,28 +9,40 @@ var addCharacter = function() {
     var listItem = document.createElement('li');					// Create the 'li' item
     listItem.setAttribute('id', number);
 
+    var upImg = document.createElement('img');
+    upImg.setAttribute('src', './img/up.png');
+
     // Move up link
     var moveUpLink = document.createElement('a');
-    moveUpLink.appendChild(document.createTextNode(' up'));
     moveUpLink.setAttribute('href', '#');                           // Set the href atribute
     moveUpLink.setAttribute('onclick', 'moveUp(this)');             // Set the onclick method
+    moveUpLink.appendChild(upImg);
+    moveUpLink.appendChild(document.createTextNode(' '));
+
+    var downImg = document.createElement('img');
+    downImg.setAttribute('src', './img/down.png');
 
     // Move Down link
     var moveDownLink = document.createElement('a');
-    moveDownLink.appendChild(document.createTextNode(' down'));
+    //moveDownLink.appendChild(document.createTextNode(' down'));
     moveDownLink.setAttribute('href', '#');                           // Set the href atribute
     moveDownLink.setAttribute('onclick', 'moveDown(this)');             // Set the onclick method
+    moveDownLink.appendChild(downImg);
+
+    var killImg = document.createElement('img');
+    killImg.setAttribute('src', './img/kill.png');
 
     // Remove link
     var removeLink = document.createElement('a');					// Create the romve link
-    removeLink.appendChild(document.createTextNode(' remove')); 	// Add the text 
+    removeLink.appendChild(document.createTextNode(' ')); 	// Add the text 
     removeLink.setAttribute('href', '#');							// Set the href atribute
     removeLink.setAttribute('onclick', 'removeCharacter(this)');	// Set the onclick method
-    
+    removeLink.appendChild(killImg);
+
     listItem.appendChild(document.createTextNode(charName));		// add the textbox var to the li
+        listItem.appendChild(removeLink);                               // add the link to the li
     listItem.appendChild(moveUpLink);
     listItem.appendChild(moveDownLink);
-    listItem.appendChild(removeLink);								// add the link to the li
     list.appendChild(listItem);										// add the li item to the list
 };
 
